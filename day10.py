eb7e894d8559961507951b402cdcb4655b25bcba
Jack Turner <jackwilliamturner@icloud.com>
2019-12-16 16:09:41 +0000

    silly jacko

--- day10.py
+++ day10.py
@@ -1,8 +1,26 @@
 from math import sqrt, isclose
 from tqdm import tqdm
+import numpy as np
+from queue import Queue
+from collections import defaultdict
 
 def distance(a,b):
     return sqrt((a[0] - b[0])**2 + (a[1] - b[1])**2)
+
+def angle(a,b,c):
+    negate = True if b[0] < c[0] else False
+
+    ## SUBTRACT CENTER POINT
+    a = a - c
+    b = b - c
+
+    ## NORMALISE VECTOR LENGTHS TO 1
+    a = 1./(np.linalg.norm(a)) * a
+    b = 1./(np.linalg.norm(b)) * b
+
+    angle = int(np.arccos(np.dot(a,b)) * 1000)
+    angle = -angle if negate else angle
+    return angle
 
 def is_between(a,c,b):
     return isclose((distance(a,c) + distance(c,b)), distance(a,b))
@@ -13,18 +31,21 @@ def int_between(a,c,b):
 
     return x1 <= c <= x2
 
-grid = []
-x, y = 0, 0
-with open('inp10.txt', 'r') as f:
-    for line in f:
-        x = 0
-        for loc in line.strip('\n'):
-            if loc == '#':
-                grid.append((x,y))
-            x += 1
-        y += 1
+def get_grid():
+    grid = []
+    x, y = 0, 0
+    with open('inputs/10', 'r') as f:
+        for line in f:
+            x = 0
+            for loc in line.strip('\n'):
+                if loc == '#':
+                    grid.append(np.array([x,y]))
+                x += 1
+            y += 1
 
-grid_width, grid_height = x, y
+    grid_width, grid_height = x, y
+
+    return grid
 
 def on_path(a, b):
 
@@ -74,36 +95,52 @@ def partA():
     # 344
     # (30, 34)
 
+def shoot(grid, p):
+    grid_ = []
+    for elem in grid:
+        if p[0] != elem[0] or p[1] != elem[1]:
+            grid_.append(elem)
+
+    return grid_
+
 def partB():
-    #starter = (30, 34)
-    # do a lap of the grid
-    #pointer = (30, 0)
+    grid = get_grid()
+    # startline
+    p_ = np.array([30,0])
+    c  = np.array([30,34])
 
-    starter = (11, 13)
-    pointer = (11, 0)
 
-    upper, lower = [(x,0) for x in range(grid_width+1)], [(x, grid_height) for x in range(grid_width)]
-    right, left  = [(grid_width, y) for y in range(grid_height)], [(0, y) for y in range(grid_height)]
-    boundaries   = upper + right + lower + left
+    grid = shoot(grid, c)
 
-    i = boundaries.index(pointer)
-    zapped = []
-    while len(zapped) < 5:
+    angles = defaultdict(lambda: [])
+    for k in grid:
+        angles[angle(p_, k,c)].append(k)
 
-        on =  on_path(starter, boundaries[i])
-        remove_me = closest(starter, on) if len(on) > 0 else None
+    # now sort all the angles by distance
+    for k,v in angles.items():
+        angles[k] = sorted(v, key=lambda x: distance(c, x))
 
-        if remove_me is not None and (len(remove_me) > 0):
-            grid.remove(remove_me)
-            zapped.append(remove_me)
 
-        if i == len(boundaries)-1:
-            i = 0
-        else:
-            i += 1
+    n_steps = 201
+    sorted_angles = sorted(angles.keys(), key=lambda x: x if x>=0 else x+10000) # there must be a better way to do this
+    ap = 0 # angle pointer
+    n  = 1
+    while(n < n_steps):
+        a = sorted_angles[ap]
+        try:
+            shootme = angles[a][0]
+            print(n, ' : ', shootme)
+            angles[a].remove(shootme)
+            grid = shoot(grid, shootme)
+            ap = ap+1 if ap+1 < len(sorted_angles) else 0
+        except:
+            ap = ap+1 if ap+1 < len(sorted_angles) else 0
+        n += 1
 
-    print(zapped)
-    #print(x, y)
 
-#partA()
+
+
+
 partB()
+
+#print(angle(np.array([3,4]), np.array([3,3]), np.array([2,3])))
--- day14.py
+++ day14.py
@@ -1,19 +1,38 @@
 import re
+from collections import defaultdict
 
-formulas = {}
-with open('things.txt', 'r') as f:
+formulas      = {}
+with open('inputs/14', 'r') as f:
     for line in f:
         lhs, rhs = line.split('=>')[0], line.split('=>')[1].strip()
         formulas[rhs] = lhs.split(',')
 
-def evaluate_cost(s):
+def evaluate_cost(s, res):
     s_= re.split('(\d+)',s.strip())
     num, mat = s_[1], s_[2]
-    
-    if 'ORE' in mat:
-        return 1
-    else:
-        return sum([evaluate_cost(i) for i in [v for k, v in formulas.items() if mat in k][0]])
 
-print(formulas)
-print(evaluate_cost('1 FUEL'))
+    # base case
+    if 'ORE' in mat:
+        return int(num)
+    # induction step 1
+    elif int(num) <= res[mat]:
+        return 0
+    # induction step n+1
+    else:
+        cost = 0
+        for k, v in formulas.items():
+            if mat in k: # then v is a list of what we need
+                # how much do we actually get?
+                num_we_get = re.split('(\d+)', k.strip())[1]
+                for vv in v: # e.g ['7 A', '1 E']
+                    cost += evaluate_cost(vv, res) # '7A'
+                    res[mat] += int(num_we_get)
+
+
+        #cost = [evaluate_cost(i, res) for i in [v for k, v in formulas.items() if mat in k][0]]
+        return cost
+
+res = defaultdict(lambda: 0)
+
+print(evaluate_cost('1 FUEL',res))
+s
